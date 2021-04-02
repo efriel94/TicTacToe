1414b5e589d096f95d4f09f0f8932bfa1018991a
Emmet Friel <emmetfriel@hotmail.co.uk>
2021-04-02 16:19:08 +0100

    Added game loop and added switch statement to check result of each move. Game works!

--- main.c
+++ main.c
@@ -12,41 +12,43 @@ int main()
     int player_turn = 1;
     int count = 0;
     int *ptr_player = &player_turn;
+    _Bool game = 0;
 
     print_title();
     char board[ROWS][COLS] = {
-        {'X' , 'X', '3'},
+        {'1' , '2', '3'},
         {'4' , '5', '6'},
         {'7' , '8', '9'}
     };
     char *ptr_board = board[0];
-    print_board(board);
 
     //loop until win or draw
-    choose_position(player_turn, ptr_board);
-    ++count;
-    system("clear");
-    print_board(board);
-    int result = win_draw(ptr_board,count);
-    switch (result) {
-      case 1:
-         fprintf(stdout, "Gamed Ended. Player %d won!\n\n", player_turn);
-         return EXIT_SUCCESS;
-         break;
-      case 2:
-         fprintf(stdout, "Gamed is a draw!\n\n");
-         return EXIT_SUCCESS;
-         break;
-      default:
-         break;
+    while (!game)
+    {
+       print_board(board);
+       choose_position(player_turn, ptr_board);
+       ++count;
+       system("clear");
+       int result = win_draw(ptr_board,count);
+       
+       //check result
+       switch (result) {
+          case 1:
+            fprintf(stdout, "Gamed Ended. Player %d won!\n\n", player_turn);
+            print_board(board);
+            return 1;
+            break;
+         case 2:
+            fprintf(stdout, "Gamed is a draw!\n\n");
+            print_board(board);
+            return 1;
+            break;
+         default:
+            change_player(ptr_player);
+            break;
+         }
     }
-    change_player(ptr_player);
     
-    choose_position(player_turn, ptr_board);
-    system("clear");
-    print_board(board);
-    change_player(ptr_player);
-
     ptr_board = NULL;
 
     return EXIT_SUCCESS;
