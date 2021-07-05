a4f5b4bcf09c7c8d10c89977355353e8b8e60a2f
Emmet Friel <emmetfriel@hotmail.co.uk>
2021-07-05 16:42:41 +0100

    Fixed play_again() function using strncmp to check if the user would like to play again

--- include/board.h
+++ include/board.h
@@ -9,6 +9,7 @@ void print_player(int *player);
 void change_player(int *player_turn);
 int win_draw(char *board, int count);
 void copy_arr(char (*src)[3], char(*dest)[3], const int size);
+_Bool play_again();
 
 
 #endif
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "board.h"
 
 #define ROWS 3
@@ -11,7 +12,8 @@ int main()
     int player_turn = 1;
     int *ptr_player = &player_turn;
     int count = 0; //keeping track of positions filled within the board
-    _Bool game = 0;
+    bool match = 0;
+    bool game = 0;
 
     print_title();
     char initialise_board[ROWS][COLS] = {
@@ -22,44 +24,52 @@ int main()
     char board[ROWS][COLS];
     copy_arr(initialise_board, board, COLS);
     char *ptr_board = board[0];
-    
-   //  fprintf(stdout, "Initialized board: ");
-   //  print_board(initialise_board);
-   //  fprintf(stdout, "Copied board: ");
-   //  print_board(board);
-    
+
     //loop until win or draw
-    while (!game)
+    while(!game)
     {
-       print_board(board);
-       choose_position(player_turn, ptr_board);
-       ++count; //
-       system("clear");
-       int result = win_draw(ptr_board,count);
-       
-       //check result
-       switch (result) {
-          case 1:
-            fprintf(stdout, "Gamed Ended. Player %d won!\n\n", player_turn);
-            print_board(board);
-            game = 1;
-            break;
-         case 2:
-            fprintf(stdout, "Gamed is a draw!\n\n");
-            print_board(board);
-            game = 1;
-            break;
-         default:
-            change_player(ptr_player);
-            break;
-         }
-    }
+      while (!match)
+      {
+         print_board(board);
+         choose_position(player_turn, ptr_board);
+         ++count;
+         system("clear");
+         int result = win_draw(ptr_board,count);
+         
+         //check result
+         switch (result) {
+            case 1:
+               fprintf(stdout, "Gamed Ended. Player %d won!\n\n", player_turn);
+               print_board(board);
+               match = 1;
+               break;
+            case 2:
+               fprintf(stdout, "Game is a draw!\n\n");
+               print_board(board);
+               match = 1;
+               break;
+            default:
+               change_player(ptr_player);
+               break;
+            }
+      }
+      bool status = play_again();
 
-   //  print_board(initialise_board);
-    
-   // //  ptr_board = NULL;
-   // //  ptr_player = NULL;
+      //If user specifies 'y' in prompt, reset and play game
+      if (status==false) {
+         copy_arr(initialise_board, board, COLS);  //reset tictactoe board
+         count = 0;                                //reset counter
+         match = 0;                                //reset match
+         system("clear");
+      } else { 
+         game = 1;                                 //exit game
+      }
+   }
+   
+   fprintf(stdout, "THANKS FOR PLAYING!");
+   ptr_board = NULL;
+   ptr_player = NULL;
 
 
-    return EXIT_SUCCESS;
+   return EXIT_SUCCESS;
 }
